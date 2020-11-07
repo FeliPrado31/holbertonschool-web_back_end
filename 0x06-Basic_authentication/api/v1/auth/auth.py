67667d0d697aa7f8c68b48495d346ec9282fec6a
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-07 02:35:44 +0000

    fix

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,11 @@ class Auth():
         """
             require authorithation check
         """
-        return False
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header check"""
