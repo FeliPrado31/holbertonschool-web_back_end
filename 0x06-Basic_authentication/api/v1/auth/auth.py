dc710de86dd9f3526cf8d2f27489e10c172485cd
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-07 02:11:58 +0000

    :sparkles: add template for all authentication system you will implement.

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+        class to manage the API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+            require authorithation check
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header check"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method"""
+        return None
