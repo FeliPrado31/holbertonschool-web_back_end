01dab30a7171d98beb27582d4d916eaa7d1e02c8
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-07 03:30:03 +0000

    :sparkles: Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""manage the API authentication"""
+"""Basic - Base64 part"""
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
@@ -13,13 +13,14 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str
                                             ) -> str:
-        """returns the Base64 part of the Authorization header"""
+        """returns the Base64 part
+        of the Authorization header for a Basic Authentication"""
         if authorization_header is None or\
            type(authorization_header) is not str:
             return None
-        hd = authorization_header.split(' ')
+        header = authorization_header.split(' ')
 
-        return hd[1] if hd[0] == 'Basic' else None
+        return header[1] if header[0] == 'Basic' else None
 
     def decode_base64_authorization_header(self,
                                            base64_authorization_header: str
