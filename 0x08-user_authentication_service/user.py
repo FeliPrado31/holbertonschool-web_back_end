ca1be0f1ec0d3db14a81491cdaee90a737ffd2e3
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 21:08:09 +0000

    fix check and add main

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -2,19 +2,21 @@
 """User model """
 
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+
 Base = declarative_base()
 
 
 class User(Base):
-    """ User class"""
-    __tablename__ = "users"
+    """
+        Create a SQLAlchemy model named User for a database table named users
+    """
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
