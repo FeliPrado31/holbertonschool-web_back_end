287fb1c87d5607bece0deb4cfbd82238e80a07ac
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 21:11:31 +0000

    :sparkles: add_user metho

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Database module
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """
+        Creates engine, session, adds user object to DB
+    """
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """save the user to the database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(u)
+        self._session.commit()
+        return user
