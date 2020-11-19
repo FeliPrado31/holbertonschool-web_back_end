aee2f577846020013ad5a60070906d6daaac083a
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 22:18:47 +0000

    :sparkles: update auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -13,6 +13,13 @@ def _hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """
+    return a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -31,3 +38,20 @@ class Auth:
         pw = _hash_password(password)
         user = self._db.add_user(email, pw)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        login validation
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            pw = _hash_password(password)
+            return bcrypt.checkpw(password.encode('utf-8'), pw)
+
+    def create_session(self, email: str) -> str:
+        """returns the session ID as a string."""
+        user = self._db.find_user_by(email=email)
+        if user:
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
