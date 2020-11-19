6f7a9d1d1b8e36d2bcaedf7e2f0a4e6aeb4261bd
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 22:16:35 +0000

    :sparkles: simpel app server

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello() -> str:
+    """welcome msg"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """registering the user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": {email}, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
