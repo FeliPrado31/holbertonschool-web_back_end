4ecf87126f04f357763175186d9b93b670136493
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-19 22:14:20 +0000

    :sparkles: try to add register_user method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -11,3 +11,23 @@ def _hash_password(password: str) -> str:
         string arguments and returns a string
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> TypeVar('User'):
+        """ hash the password with _hash_password
+            save the user to the database using self._db
+            return the User object
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError('User {email} already exists')
+        pw = _hash_password(password)
+        user = self._db.add_user(email, pw)
+        return user
