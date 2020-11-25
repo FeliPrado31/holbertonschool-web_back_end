79c3881586e08e15e02275bc89936c9cd054040d
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-25 03:30:38 +0000

    submid test

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -19,3 +19,39 @@ class TestGithubOrgClient(unittest.TestCase):
         test_return = test_client.org
         self.assertEqual(test_return, mock_get.return_value)
         mock_get.assert_called_once
+
+    def test_public_repos_url(self):
+        """ test_public_repos_url"""
+        with patch.object(GithubOrgClient,
+                          "org",
+                          new_callable=PropertyMock,
+                          return_value={"repos_url": "twitter"}) as mock_get:
+            test_json = {"repos_url": "twitter"}
+            test_client = GithubOrgClient(test_json.get("repos_url"))
+            test_return = test_client._public_repos_url
+            mock_get.assert_called_once
+            self.assertEqual(test_return,
+                             mock_get.return_value.get("repos_url"))
+
+    @patch("client.get_json", return_value=[{"name": "twitter"}])
+    def test_public_repos(self, mock_get):
+        """test_public_repos"""
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_callable=PropertyMock,
+                          return_value="https://api.github.com/") as mock_pub:
+            test_client = GithubOrgClient("twitter")
+            test_return = test_client.public_repos()
+            self.assertEqual(test_return, ["twitter"])
+            mock_get.assert_called_once
+            mock_pub.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_return):
+        """test_has_license"""
+        test_client = GithubOrgClient("twitter")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(expected_return, test_return)
