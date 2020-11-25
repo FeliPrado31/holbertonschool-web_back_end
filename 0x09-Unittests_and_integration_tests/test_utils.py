55cdf4c793c2876757233e28631c123a3a6fc2af
Feli <55117998+FeliPrado31@users.noreply.github.com>
2020-11-25 03:02:07 +0000

    :sparkles: add 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, answer):
         """ test that the method returns what it is supposed to"""
         self.assertEqual(access_nested_map(nested_map, path), answer)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ test that a KeyError is raised properly"""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
