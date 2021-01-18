4867b4917d5379336592480738f4be3ee3ec05d6
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-01-18 00:00:12 +0000

    :sparkles: rounded test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -6,6 +6,13 @@ describe("calculateNumber", () => {
     assert.equal(calculateNumber(1.1, 2.5), 4);
     assert.equal(calculateNumber(0.0, 0), 0);
   });
+  it("should round floats", () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(0.1, 0), 0);
+    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+  });
   it("negative numbers", () => {
     assert.equal(calculateNumber(-1, 1), 0);
     assert.equal(calculateNumber(-1.5, 0), -1);
