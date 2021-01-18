ad21f69cb58afa2e9515f8cc8caba0202cf3c1bc
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-01-18 00:28:11 +0000

    :sparkles: fix

--- 0x13-unittests_in_js/8-api/api.js
+++ 0x13-unittests_in_js/8-api/api.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 7865;
 
-app.get("/", (_, res) => res.send("Welcome to the payment system"));
+app.get("/", (req, res) => {
+  res.end("Welcome to the payment system");
+});
 
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+app.listen(port, () => {
+  console.log("API available on localhost port 7865");
+});
--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 7865;
 
-app.get("/", (_, res) => res.send("Welcome to the payment system"));
+app.get("/", (req, res) => res.send("Welcome to the payment system"));
 
 app.get("/cart/:id([0-9]+)", (req, res) =>
   res.send(`Payment methods for cart ${req.params.id}`)
