c0096de57df7e63b85b3d88628ba052cf3dfd9a4
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-01-18 00:05:27 +0000

    fix

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,6 +2,11 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber", () => {
+  it("should return sum of integers", () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, -1), 0);
+    assert.strictEqual(calculateNumber(1, -3), -2);
+  });
   it("checks the output", () => {
     assert.equal(calculateNumber(1.1, 2.5), 4);
     assert.equal(calculateNumber(0.0, 0), 0);
@@ -12,6 +17,7 @@ describe("calculateNumber", () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(0.1, 0), 0);
     assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+    assert.strictEqual(calculateNumber(1, "3.7"), 5);
   });
   it("negative numbers", () => {
     assert.equal(calculateNumber(-1, 1), 0);
