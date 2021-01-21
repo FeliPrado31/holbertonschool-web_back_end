7e717cd4a1b1691aa307c7380a36e480591204c6
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-01-21 03:43:25 +0000

    :sparkles: add status

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -9,3 +9,11 @@ const data = {
 const job = queue.create("push_notification_code", data).save((err) => {
   if (!err) console.log(`Notification job created: ${job.id}`);
 });
+
+job
+  .on("complete", () => {
+    console.log("Notification job completed");
+  })
+  .on("failed", () => {
+    console.log("Notification job failed");
+  });
