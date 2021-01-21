9d9063126c37ae6a7f14958437bf9bd73fa1684b
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-01-21 04:24:39 +0000

    :sparkles: add

--- 0x14-queuing_system_in_js/7-job_creator.js
+++ 0x14-queuing_system_in_js/7-job_creator.js
@@ -48,16 +48,22 @@ const jobs = [
 ];
 
 const queue = kue.createQueue();
+const queueKey = "push_notification_code_2";
 
-for (const j of jobs) {
-  const job = queue.create("push_notification_code", j).save((err) => {
+jobs.forEach((jobObj) => {
+  const job = queue.create(queueKey, jobObj).save((err) => {
     if (!err) console.log(`Notification job created: ${job.id}`);
   });
-  job.on("complete", () => console.log(`Notification job ${job.id} completed`));
-  job.on("failed", (error) =>
-    console.log(`Notification job ${job.id} failed: ${error}`)
-  );
-  job.on("progress", (progress) =>
-    console.log(`Notification job ${job.id} ${progress}% complete`)
-  );
-}
+
+  job.on("complete", () => {
+    console.log(`Notification job ${job.id} completed`);
+  });
+
+  job.on("failed", (errorMessage) => {
+    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+  });
+
+  job.on("progress", (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
